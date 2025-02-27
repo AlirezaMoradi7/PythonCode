3344949771f359ebfe01511202ff2149d10ed395
Alireza <Alireza.moradi25@gmail.com>
2025-02-27 13:57:44 +0330

    Session1

---
+++ Session1.py
@@ -0,0 +1,51 @@
+# v = input ("Please type your name :")
+# print(int(v)+10)
+from datetime import datetime
+from itertools import count
+
+# print(F"nosha sajad .....:{v}")
+
+# name = '   Sajad kolivand babak   '
+# print(name)
+# print(name.lstrip())
+# print(name.split(sep='*'))
+# print(name.rsplit(sep='*'))
+# print(''.join(name.split()))
+# tup = (10,'15',112)
+# names = ['sajad','zahra','nusha']
+# values = [10,200,112,10]
+
+# values.sort()
+# print(values[-1])
+# print(values[-2:-1])
+# print(values.count(10))
+# print(len(values))
+# for i in values:
+#     print(i)
+# from itertools import zip_longest
+# print(zip(names,values))
+# print(list(zip(names,values)))
+# print(list(zip_longest(names,values,fillvalue="babak")))
+
+# -----------------------------------------
+# values=['45TV_','AM_780','RS786','12@','vahid7740']
+# # values='45TV_154ll'
+# # print(lambda sajad : ''.join([i for i in sajad if i.isdigit()]))
+# print(list(map(lambda sajad : ''.join([i for i in sajad if i.isdigit()]),values)))
+#
+
+# myValue="Christy Zhu 1973-08-14"
+# f = myValue.split()[0]
+# l = myValue.split()[1]
+# a = myValue.split()[2].split(sep="-")[0]
+# y = datetime.now().year
+# print(f,l,a,y)
+
+
+
+
+
+
+
+
+
